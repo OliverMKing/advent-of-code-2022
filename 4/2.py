61c2f73da0dc21520211d9e118d703b04a6edb04
Oliver King <olivermerkleyking@gmail.com>
2022-12-04 11:43:41 -0500

    add day 4

---
+++ 4/2.py
@@ -0,0 +1,25 @@
+import fileinput
+
+def parsedLine(line: str):
+    cleaned = line.strip()
+    elves = cleaned.split(',')
+    return [map(int, elf.split('-')) for elf in elves]
+
+if __name__ == '__main__':
+    total = 0
+
+    splitInput = [parsedLine(line) for line in fileinput.input()]
+    for elf1, elf2 in splitInput:
+        elf1Min, elf1Max = elf1
+        elf2Min, elf2Max = elf2
+
+        if elf1Max >= elf2Max >= elf1Min:
+            total += 1
+        elif elf1Max >= elf2Min >= elf1Min:
+            total += 1
+        elif elf2Max >= elf1Max >= elf2Min:
+            total += 1
+        elif elf2Max >= elf1Min >= elf2Min:
+            total += 1
+        
+    print(total)
