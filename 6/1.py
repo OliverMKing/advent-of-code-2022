54fc6f713ed6e3e4b48af425918cc3267aaeaf51
Oliver King <olivermerkleyking@gmail.com>
2022-12-06 21:22:07 -0500

    add day 6 solution

---
+++ 6/1.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+signal = input().strip()
+latest = defaultdict(lambda: 0)
+lastN = 4
+currDuplicates = 0
+
+for i, char in enumerate(signal):
+    latest[char] += 1
+
+    if latest[char] == 2:
+        currDuplicates += 1
+    
+    removeI = i - lastN
+    if removeI < 0:
+        continue
+    
+    removeChar = signal[removeI]
+    latest[removeChar] -= 1
+    if latest[removeChar] == 1:
+        currDuplicates -= 1
+    
+    if currDuplicates == 0:
+        print(i + 1)
+        break
